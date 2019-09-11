3c6870e3a43eebb695d7b30ba9de817800c2f51e
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-09-11 21:35:50 +0300

    Update Task_1.java

--- src/com/company/hw_2/task_1/Task_1.java
+++ src/com/company/hw_2/task_1/Task_1.java
@@ -2,15 +2,18 @@ package com.company.hw_2.task_1;
 
 public class Task_1 {
     public static void main(String[] args) {
+        /* задание пока не выполнено до конца, еще должно быть
+        x = myDouble; 
+        */
         int x=1;
         double myDouble = x;
         System.out.println("myDouble=" + myDouble);
 
-
+// не оставляй пустые строки
 
 
 
     }
 
-
+// не оставляй пустые строки
 }
